63f46afc6240948ac5eeb4cf313d32b429012df8
S <s>
2021-03-30 23:01:05 +0400

    {"types":"C","runs":9}

--- CheckersGraphics.java
+++ CheckersGraphics.java
@@ -35,7 +35,6 @@ public class CheckersGraphics extends GCanvas {
 			add(outer_circle, (width - outer_diameter)/2, (height - outer_diameter)/2);
 			add(inner_cirlce, (width - inner_diameter)/2, (height - inner_diameter)/2);
 			
-
 			checker.setFilled(true);
 			checker.setFillColor(checker_color);
 		}
