193926d60ccb2b9d0ab022a994763de8a92a9395
S <s>
2021-03-30 22:58:54 +0400

    {"types":"C","runs":9}

--- CheckersGraphics.java
+++ CheckersGraphics.java
@@ -110,7 +110,7 @@ public class CheckersGraphics extends GCanvas {
 		double square_height = height/N_COLS;
 		
 		
-		add(checker, j * square_width, i * height);
+		add(checker, j * square_width, i * square_height);
 	}
 	
 	private double min(double first, double second) {
