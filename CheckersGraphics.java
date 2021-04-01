062643aebf4129e0a6d1c66730bbdeb497062c04
S <s>
2021-04-01 22:43:56 +0400

    {"types":"C","runs":10}

--- CheckersGraphics.java
+++ CheckersGraphics.java
@@ -22,8 +22,8 @@ public class CheckersGraphics extends GCanvas {
 	public class Checker extends GCompound {
 		
 		static final double OUTER_SCALE = 0.7;
-		static final double INNER_SCALE = 0.40;
-		static final double MID_SCALE = 0.5;
+		static final double INNER_SCALE = 0.4;
+		static final double MID_SCALE = 0.45;
 		
 		public Checker(double width, double height, boolean dark) {
 
