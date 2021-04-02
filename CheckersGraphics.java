664d3a4ada95519ee9c88c815c5190bf8e8c7da5
S <s>
2021-04-02 23:06:16 +0400

    {"types":"C","runs":29}

--- CheckersGraphics.java
+++ CheckersGraphics.java
@@ -234,7 +234,7 @@ public class CheckersGraphics extends GCanvas {
 	
 	private void highlightSquare(int row, int col, boolean light) {
 		
-		if(light == )
+		if(light == highlight[row][col])
 	}
 	
 	private double min(double first, double second) {
