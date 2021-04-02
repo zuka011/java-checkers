a75a0e3b7872e00b6054f480509592285526c22f
S <s>
2021-04-02 23:38:05 +0400

    {"types":"C","runs":44}

--- CheckersGraphics.java
+++ CheckersGraphics.java
@@ -258,7 +258,7 @@ public class CheckersGraphics extends GCanvas {
 
 			if(checkers[row][col] != EMPTY) {
 				
-				int player = 
+				int checker_type = checkers[row][col]
 				add(highlighter_rect, col * square_width, row * square_height);
 			
 			} else {
