2563b14b46bbbd2033fb65baddb11ad4468cd136
S <s>
2021-04-02 23:17:18 +0400

    {"types":"C","runs":29}

--- CheckersGraphics.java
+++ CheckersGraphics.java
@@ -259,7 +259,7 @@ public class CheckersGraphics extends GCanvas {
 			highlighter_rect.sendBackward();
 		} else {
 			
-			GRect highlighter_rect = getElementAt(col * square_width + 1, row * square_height + 1)
+			GRect highlighter_rect = getElementAt(col * square_width + 1, row * square_height + 1);
 		}
 	}
 	
