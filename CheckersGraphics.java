b4f9510d2651dffa566e0b83c4fc811a1171ae3b
S <s>
2021-04-02 21:27:44 +0400

    {"types":"C","runs":16}

--- CheckersGraphics.java
+++ CheckersGraphics.java
@@ -105,7 +105,7 @@ public class CheckersGraphics extends GCanvas {
 	
 	public void initialize(boolean reverse) {
 		
-		this.addMouseListener(null);
+		this.addMouseListener(mouse_listener);
 		
 		drawBoard();
 		
