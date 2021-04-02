c1d638560d7063f24c10ab054eb7dc77f97b8ef1
S <s>
2021-04-02 21:45:43 +0400

    {"types":"C","runs":19}

--- CheckersGraphics.java
+++ CheckersGraphics.java
@@ -189,7 +189,7 @@ public class CheckersGraphics extends GCanvas {
 		double x = square_width * j + square_width / 2;
 		double y = square_height * i + square_height / 2;
 		
-		
+		remove(getElementAt(x, y));
 		
 	}
 	
