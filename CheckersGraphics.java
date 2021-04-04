5ad0bc7c24e3d3740fc71baa7c80c8bd0a3b2bd9
S <s>
2021-04-04 22:44:51 +0400

    {"types":"C","runs":18}

--- CheckersGraphics.java
+++ CheckersGraphics.java
@@ -57,6 +57,15 @@ public class CheckersGraphics extends GCanvas {
 		}
 				
 		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + col;
+			result = prime * result + row;
+			return result;
+		}
+		
+		@Override
 	    public boolean equals(Object o)  {
 			
 			Coordinate other_coordinate = (Coordinate) o;
