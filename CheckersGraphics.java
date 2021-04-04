dc173676df26474ce03fba9f4de3e9654b6b039d
S <s>
2021-04-04 21:58:40 +0400

    {"types":"C","runs":5}

--- CheckersGraphics.java
+++ CheckersGraphics.java
@@ -238,7 +238,7 @@ public class CheckersGraphics extends GCanvas {
 		}
 	}
 	
-	private	Coordinate[]
+	private	Coordinate[] 
 	
 	private void highlightSquare(int row, int col, boolean light) {
 		
