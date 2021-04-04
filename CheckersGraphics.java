f569e7ce0d2e044592edbf68e1410ab76cbc0785
S <s>
2021-04-04 22:00:57 +0400

    {"types":"C","runs":5}

--- CheckersGraphics.java
+++ CheckersGraphics.java
@@ -238,7 +238,7 @@ public class CheckersGraphics extends GCanvas {
 		}
 	}
 	
-	private	Coordinate[] possibleMoves(int start_row, int start_col) {
+	private	HashSet<Coordinate> possibleMoves(int start_row, int start_col) {
 		
 	}
 	
