60c000de30b7a5b202f11e2c4a0b8f9e9978e5cb
S <s>
2021-04-04 22:26:41 +0400

    {"types":"C","runs":9}

--- CheckersGraphics.java
+++ CheckersGraphics.java
@@ -256,7 +256,7 @@ public class CheckersGraphics extends GCanvas {
 			
 		case FIRST_PLAYER:
 
-			if(inBounds(start_row + diff_row, start_col - 1) && checkers[]) possible_moves.add(new Coordinate(start_row + diff_row, start_col - 1));
+			if(inBounds(start_row + diff_row, start_col - 1) && checkers[start_row + diff_row][]) possible_moves.add(new Coordinate(start_row + diff_row, start_col - 1));
 			if(inBounds(start_row + diff_row, start_col + 1)) possible_moves.add(new Coordinate(start_row + diff_row, start_col + 1));
 			
 			return possible_moves;
