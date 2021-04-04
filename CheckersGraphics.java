0c41dceaaf757693ffe140d04cf2c40ea11ba0c9
S <s>
2021-04-04 22:45:06 +0400

    {"types":"C","runs":18}

--- CheckersGraphics.java
+++ CheckersGraphics.java
@@ -58,7 +58,7 @@ public class CheckersGraphics extends GCanvas {
 				
 		@Override
 		public int hashCode() {
-			final int prime = 31;
+			final int row = 31;
 			int result = 1;
 			result = prime * result + col;
 			result = prime * result + row;
