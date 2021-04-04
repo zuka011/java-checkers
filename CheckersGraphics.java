cd0f4055cf5b9ac2361c82079155b5eb4959d346
S <s>
2021-04-04 21:38:37 +0400

    {"types":"C","runs":0}

--- CheckersGraphics.java
+++ CheckersGraphics.java
@@ -57,7 +57,7 @@ public class CheckersGraphics extends GCanvas {
 		static final double INNER_SCALE = 0.4;
 		static final double MID_SCALE = 0.47;
 		
-		public Checker(double width, double height, boolean dark) {
+		public Checker(double width, double height, final int player) {
 
 			double outer_diameter = min(width, height) * OUTER_SCALE;
 			double inner_diameter = min(width, height) * INNER_SCALE;
